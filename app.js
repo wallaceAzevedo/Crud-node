7c522caa2dc3c4f0008f595bc9608adc63d653ba
wallaceAzevedo <wallace_azevedo17@hotmail.com>
2021-06-17 10:01:05 -0300

    conexão
    conexão bem sucedida com o banco de dados test

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Ligar á B.D.: 'test'->user da BD, ´nnn´->pass
+mongoose.connect('mongodb+srv://test:nnn@nodejscluster-art2k.mongodb.net/test?retryWrites=true');
+
+// Confirma ligação na consola
+mongoose.connection.on('connected', function () {
+  console.log('Connected to Database' +'test');
+});
+
+// Mensagem de Erro
+mongoose.connection.on('error', (err) => {
+  console.log('Database error' +err);
+});
 
 const app = express();
 
